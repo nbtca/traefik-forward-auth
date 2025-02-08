32576e901695c770b78d40b95d0ae81c1c94980c
LazuliKao <LazuliKao233@outlook.com>
2025-02-08 16:01:47 +0800

    add role scope

--- internal/provider/oidc.go
+++ internal/provider/oidc.go
@@ -50,7 +50,7 @@ func (o *OIDC) Setup() error {
 		Endpoint:     o.provider.Endpoint(),
 
 		// "openid" is a required scope for OpenID Connect flows.
-		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email", "role"},
 	}
 
 	// Create OIDC verifier
