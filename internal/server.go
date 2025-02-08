0a8445793484b75084bdeb687e1c006f257d45c8
LazuliKao <LazuliKao233@outlook.com>
2025-02-08 15:09:40 +0800

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -192,6 +192,7 @@ func (s *Server) AuthCallbackHandler() http.HandlerFunc {
 			"provider": providerName,
 			"redirect": redirect,
 			"user":     user.Email,
+			"roles":    user.Roles,
 		}).Info("Successfully generated auth cookie, redirecting user.")
 
 		// Redirect
