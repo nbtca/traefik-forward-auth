4cd0a2bbce8294179331ccbf4bc24b7215699e5b
LazuliKao <LazuliKao233@outlook.com>
2025-02-08 14:47:59 +0800

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -179,6 +179,7 @@ func (s *Server) AuthCallbackHandler() http.HandlerFunc {
 
 		// Get user
 		user, err := p.GetUser(token)
+		logger.WithField("user", user.Email).WithField("roles", user.Roles).Info("Got user")
 		if err != nil {
 			logger.WithField("error", err).Error("Error getting user")
 			http.Error(w, "Service unavailable", 503)
